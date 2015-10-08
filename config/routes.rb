0d11c3bc46e30a6c1c9bc518de6f74b04fed1ec9
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2015-10-07 21:13:10 -0500

    Assignment 35 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+
+  resources :posts, :advertisements
+  
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,13 @@ include RandomData
 
 
 50.times do
-  Post.create!(
+  Advertisement.create!(
     title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    copy: RandomData.random_paragraph,
+    price: 1
   )
 end
-posts = Post.all
-
-
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+advertisements = Advertisement.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
