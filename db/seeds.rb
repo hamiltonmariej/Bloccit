ab91006cb668dbd61850151ebbaa0fdd6961e102
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2015-10-11 21:03:41 -0500

    Assignment 36 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,12 @@ include RandomData
 
 
 50.times do
-  Post.create!(
+  Question.create!(
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
-posts = Post.all
-
-
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+questions = Question.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
