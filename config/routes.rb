18b83af85013f2592208d6f2ff95f78a8f448c15
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2015-10-13 22:39:25 -0500

    Progress on assignment 37

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index] do
+      resources :sponsored_posts
+    end
   end
-  
+
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
