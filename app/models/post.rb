67cf5f5bfccc2a7229bdc886892ad8c2b3d854c5
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2015-11-29 21:35:36 -0600

    Assignment Bloccit votingcorrection-still not working

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -31,11 +33,9 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
-  after_create :create_vote
-
   private
 
   def create_vote
-    user.votes.create!(value: 1, post: post)
+    user.votes.create(value: 1, post: self)
   end
 end
